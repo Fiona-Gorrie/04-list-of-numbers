b4fd1582260d8fd98e57c0b8501c23ff2bd55e6e
Fiona-Gorrie <47618889+Fiona-Gorrie@users.noreply.github.com>
2019-04-11 21:28:39 -0600

    Update num_list.py

--- num_list.py
+++ num_list.py
@@ -2,26 +2,16 @@
 nums = [12, 10, 32, 3, 66, 17, 42, 99, 20]
 
 def print_each_number():
-    # Write a loop that prints each of the numbers on a new line, like this:
-    # 12
-    # 10
-    # ...etc
-    # TODO - Write your code below this line.  (Keep the indentation)
-    for i in nums:
-        print(i)
-
-    
+    for num in nums:
+        print(num)
+    return
 
 def print_each_number_and_its_square():
-    # Write a second loop that prints each number and its square on a new line
-    # precisely like this:
-    #
-    # The square of 12 is 144
-    # The square of 10 is 100
-    # ...etc
-    # TODO - Write your code below this line.  (Keep the indentation)
-    nums = [12, 10, 32, 3, 66, 17, 42, 99, 20]
+    for num in nums:
+        print(num**2) 
+    return
 
-        for i in nums:
-            print("The square of ", str(i), " is ", str(i))
 
+
+
+            
